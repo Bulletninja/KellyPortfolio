3fb42b94500dc67e0d1db6af26d479d79e315943
Bulletninja <luis.linuxero@gmail.com>
2023-06-09 00:47:51 +0000

    Some placeholders

---
+++ estimators/__init__.py
@@ -0,0 +1,23 @@
+""" 
+Estimates expected annual excess returns for a given asset from PEG ratios,
+earnings per share, and growth rates using monte carlo simulation.
+"""
+
+def get_peg_ratios(growth_rates, assets):
+    """Gets the PEG ratios for the assets"""
+
+    # Gets the earnings per share
+    eps = get_eps(prices, assets)
+
+    # Gets the growth rates
+    growth_rates = get_growth_rates(eps, assets)
+
+    # Gets the PEG ratios
+    peg_ratios = get_peg_ratios(growth_rates, assets)
+
+    return peg_ratios
+
+def get_excess_expected_returns(peg_ratios, assets):
+    """Runs montecarlo to estimate distribution of expected returns"""
+
+
--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,1 +1,41 @@
 """Sieves stocks based on Value Investing principles"""
+
+import os
+import sys
+import time
+import json
+import datetime
+import argparse
+import numpy as np
+import pandas as pd
+import yfinance
+from cvxopt import matrix
+from cvxopt.solvers import qp
+from sklearn.covariance import LedoitWolf
+from typing import Dict
+
+def get_good_deal_assets():
+    """Gets the assets that are good deals based on the value investing principles"""
+
+    assets = load_config('config.json') # dataframes of assets
+    prices = load_prices(assets) # dataframes of prices
+    
+    # Gets the assets that are cheap based on the value investing principles
+    peg_ratios = get_peg_ratios(prices, assets)
+    good_deal_assets = peg_ratios[peg_ratios['PEG'] <= 1]
+    return good_deal_assets
+
+
+def get_peg_ratios(prices, assets):
+    """Gets the PEG ratios for the assets"""
+
+    # Gets the earnings per share
+    eps = get_eps(prices, assets)
+
+    # Gets the growth rates
+    growth_rates = get_growth_rates(eps, assets)
+
+    # Gets the PEG ratios
+    peg_ratios = get_peg_ratios(growth_rates, assets)
+
+    return peg_ratios
